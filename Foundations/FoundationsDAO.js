f46a6750b85eb62f87e16c07776461e77c224434
Gustavogamboa96 <Gustavo.gamboa96@gmail.com>
2024-09-18 08:57:37 -0400

    design doc and prelim DAO

---
+++ Foundations/FoundationsDAO.js
@@ -0,0 +1,132 @@
+const {DynamoDBClient} = require("@aws-sdk/client-dynamodb");
+const uuid = require("uuid");
+const {DynamoDBDocumentClient, 
+        GetCommand,
+        PutCommand,
+        UpdateCommand,
+        DeleteCommand,
+        ScanCommand,
+        QueryCommand} = require("@aws-sdk/lib-dynamodb");
+
+
+const client = new DynamoDBClient({region: "us-east-1"});
+
+const documentClient = DynamoDBDocumentClient.from(client);
+
+const TableName = "";
+
+
+async function getList(){
+    const command  = new ScanCommand({
+        TableName
+    })
+    try {
+        const data = await documentClient.send(command);
+        return data.Items;
+    } catch (err) {
+        console.error(err);
+        throw err; 
+    }
+}
+// console.log(getList());
+async function addItem(Item){
+    let item_id = uuid.v4();
+    Item["item-id"] = item_id;
+
+    const command = new PutCommand({
+        TableName,
+        Item
+    });
+    try {
+        const data = await documentClient.send(command);
+        return data
+        
+    } catch (err) {
+        console.error(err);
+        throw err; 
+    }
+
+}
+
+async function findByName(itemName){
+    const command = new QueryCommand({
+        TableName: "groceries",
+        IndexName: "name-index", // GSI name
+        KeyConditionExpression: "#n = :name",
+        ExpressionAttributeNames: {
+            "#n" : "name"
+        },
+        ExpressionAttributeValues: {
+            ":name": itemName
+        }
+    });
+    try {
+        const data = await documentClient.send(command);
+        if (data.Items && data.Items.length > 0) {
+            return data.Items[0]; // Assuming there's only one match
+        } else {
+            throw new Error("Item not found");
+        }
+    } catch (err) {
+        console.error("Error querying the item:", err);
+        throw err;
+    }
+}
+
+async function updateItem(Item){
+    const itemToUpdate = await findByName(Item.name);
+    const key = {"item-id": itemToUpdate["item-id"]};
+    let updateExpression = 'set ';
+    const expressionAttributeValues = {};
+    const expressionAttributeNames = {};
+
+    Object.keys(Item).forEach((attribute, index) => {
+        const attrName = `#attr${index}`;
+        updateExpression += `${attrName} = :${attribute}`;
+
+        if(index < Object.keys(Item).length -1){
+            updateExpression += ', ';
+        }
+
+        expressionAttributeValues[`:${attribute}`] = Item[attribute];
+        expressionAttributeNames[attrName] = attribute;
+    })
+
+    const command  = new UpdateCommand({
+        TableName,
+        Key: key,
+        UpdateExpression: updateExpression,
+        ExpressionAttributeValues: expressionAttributeValues,
+        ExpressionAttributeNames: expressionAttributeNames,
+        ReturnValues: "ALL_NEW"
+    });
+    try {
+        const data = await documentClient.send(command);
+        return data.Attributes;
+        
+    } catch (err) {
+        console.error(err);
+        throw err; 
+    }    
+}
+
+async function removeItem(Item){
+    const itemToDelete = await findByName(Item.name);
+    const key = {"item-id": itemToDelete["item-id"]};
+
+    const command = new DeleteCommand({
+        TableName,
+        Key: key,
+    })
+    try {
+       const data = documentClient.send(command);
+       return data;
+    } catch (err) {
+        console.error(err);
+        throw err; 
+    }
+}
+
+module.exports = {
+    getList, addItem, updateItem, removeItem
+    };
---
+++ Foundations/README.md
@@ -0,0 +1,75 @@
+# FOUNDATIONS PROJECT
+Link: [Link to this doc](#)
+
+Author(s): Gustavo Gamboa Malaver
+
+Status: [Draft]
+
+Last Updated: 2024-09-18
+
+## Contents
+- Goals
+- Non-Goals
+- Background
+- Overview
+- Detailed Design
+  - Solution 1
+    - Backend
+- Considerations
+- Metrics
+
+## Objective
+This is a backend project to leverage the NED stack
+Node, Express,DynamoDB using the AWS SDK and
+Unit testing with Jest
+
+## Goals
+- To develop a ticketing reimbursement system for a company 
+## Non-Goals
+- Non-Goals
+
+## Background
+This is a project completed in Revature training.
+
+## Overview
+This project should allow employees to create tickets detailing their expenses
+and should allow managers to see said tickets and approve them or deny them.
+
+## Detailed Design
+_Use diagrams where necessary._
+
+_Tools like [Excalidraw](https://excalidraw.com) are good resources for this._
+
+_Cover major changes:_
+
+ _- What are the new functions that you will write?_
+
+ _- Why do you need new components?_
+
+ _- Is there any code that can be reusable?_
+
+_Don’t elaborate deeply on the implementation details._
+
+## Solution 1
+### Backend
+_Backend…_
+
+## Considerations
+_Concerns / trade-offs / tech debt_
+
+## Metrics
+_What data do you need to validate before launching this feature?_
+
+
+
+
+
+
+
+
+
+
+
+
+
+
